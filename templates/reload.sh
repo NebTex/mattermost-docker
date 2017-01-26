4d31eb009c698a92fadc86703143ef93a82ccf28
Cristian Lozano <criscalovis@gmail.com>
2017-01-26 06:23:16 -0500

    fix consul template

--- templates/reload.sh
+++ templates/reload.sh
@@ -12,8 +12,9 @@ DB_PASSWORD={{ .Data.password }}
 {{ end }}
 {{ end }}
 
-DB_HOST={{ printf "Settings/%s/AppRoles/%s/DB/HOST" (env "NAMESPACE") (env "APP_NAME") | key }} 
-DB_PORT={{ printf "Settings/%s/AppRoles/%s/DB/PORT" (env "NAMESPACE") (env "APP_NAME") | key }} 
+DB_HOST={{ printf "Settings/NameSpaces/%s/AppRoles/%s/DB/HOST" (env "NAMESPACE") (env "APP_NAME") | key }} 
+DB_PORT={{ printf "Settings/NameSpaces/%s/AppRoles/%s/DB/PORT" (env "NAMESPACE") (env "APP_NAME") | key }} 
+DB_NAME={{ printf "Settings/NameSpaces/%s/AppRoles/%s/DB/NAME" (env "NAMESPACE") (env "APP_NAME") | key }} 
 
 config=/mattermost/config/config.json
 
@@ -21,10 +22,12 @@ sed -Ei "s/DB_USERNAME/${DB_USERNAME}/" $config
 sed -Ei "s/DB_PASSWORD/${DB_PASSWORD}/" $config
 sed -Ei "s/DB_HOST/${DB_HOST}/" $config
 sed -Ei "s/DB_PORT/${DB_PORT}/" $config
+sed -Ei "s/DB_NAME/${DB_NAME}/" $config
 sed -Ei "s/AWS_ACCESS_KEY_ID/${AWS_ACCESS_KEY_ID}/" $config
 sed -Ei "s/AWS_SECRET_ACCESS_KEY/${AWS_SECRET_ACCESS_KEY}/" $config
 
 echo ${DB_HOST:?} > /etc/db_host
 echo ${DB_PORT:?} > /etc/db_port
+echo ${DB_NAME:?} > /etc/db_NAME
 
 supervisorctl restart mattermost
